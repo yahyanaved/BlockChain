889968f389e0de04904db6faff80cda94bc4d45e
yahyanaved <yahya.naved4321@gmail.com>
2021-12-02 22:48:36 +0500

    100% final

--- source.cpp
+++ source.cpp
@@ -5,153 +5,211 @@
 #include<ctime>
 #include<chrono>
 #include"json.hpp"
+#pragma warning(disable:4996)
 using json = nlohmann::json;
 using namespace std;
-int miner = -1;
-int answer = -1;
-int var = 0;
-string NowToString()
-{
-    chrono::system_clock::time_point p = chrono::system_clock::now();
-    time_t t = chrono::system_clock::to_time_t(p);
-    char str[26];
-    ctime_s(str, sizeof str, &t);
-    return str;
-}
-class jar
+class miscellaneous
 {
 public:
-    long noofpills;
-    int pillsweight;
-    jar()
+    string NowToString()
     {
-
+        chrono::system_clock::time_point p = chrono::system_clock::now();
+        time_t t = chrono::system_clock::to_time_t(p);
+        char str[26];
+        ctime_s(str, sizeof str, &t);
+        return str;
     }
-    void input(int a, int b)
+    int randoms(int min, int max)
     {
-        noofpills = a;
-        pillsweight = b;
-    }
-};
-int randoms(int min, int max)
-{
-    static bool first = true;
-    if (first)
-    {
-        srand(time(NULL)); 
-        first = false;
-    }
-    return min + rand() % ((max + 1) - min);
-}
-void verify(jar * obj)
-{
-    if (obj[answer-1].pillsweight == 9)
-    {
-        var++;
-    }
-
-}
-void p1(jar* obj, long noofjars, int a)
-{
-    long sum = 0;
-    for (long i = 0; i < noofjars;i++)
-    {
-        sum = sum + (obj[i].pillsweight * (i + 1));
-    }
-    long actualsum = 0;
-    for (long i = 0;i < noofjars;i++)
-    {
-        actualsum = actualsum + ((i + 1) * 10);
-    }
-    long index = actualsum - sum;
-    if (miner == -1)
-    {
-        miner = a;
-        answer = index;
-    }
-    else
-    {
-        verify(obj);
-    }
-}
-jar* pill(long &noofjars)
-{
-    noofjars = randoms(100000, 150000);
-    long nonce = randoms(0, noofjars-1);
-    jar* jars = new jar[noofjars];
-    for (long i = 0; i < noofjars;i++)
-    {
-        if (i == nonce)
+        static bool first = true;
+        if (first)
         {
-            jars[i].input(150000, 9);
+            srand(time(NULL));
+            first = false;
+        }
+        return min + rand() % ((max + 1) - min);
+    }
+}misc;
+class puzzle
+{
+public:
+    friend class miscellaneous;
+    friend class mining;
+    int miner;
+    int answer;
+    int var;
+    puzzle()
+    {
+        miner = -1;
+        answer = -1;
+        var = 0;
+    }
+    class jar
+    {
+    public:
+        long noofpills;
+        int pillsweight;
+        jar()
+        {
+
+        }
+        void input(int a, int b)
+        {
+            noofpills = a;
+            pillsweight = b;
+        }
+    };
+    void p1(jar*obj, long noofjars, int a)
+    {
+        long sum = 0;
+        for (long i = 0; i < noofjars;i++)
+        {
+            sum = sum + (obj[i].pillsweight * (i + 1));
+        }
+        long actualsum = 0;
+        for (long i = 0;i < noofjars;i++)
+        {
+            actualsum = actualsum + ((i + 1) * 10);
+        }
+        long index = actualsum - sum;
+        if (miner == -1)
+        {
+            miner = a;
+            answer = index;
         }
         else
         {
-            jars[i].input(150000, 10);
+            verify(obj);
         }
     }
-    return jars;
-}
-void puzzle()
-{
-    long noofjars = 0;
-    jar* obj = pill(noofjars);
-    int x = randoms(1, 4);
-    thread t1(p1, obj, noofjars,1);
-    thread t2(p1, obj, noofjars,2);
-    thread t3(p1, obj, noofjars,3);
-    t1.join();
-    t2.join();
-    t3.join();
-
-}
-void datapool(json &obj)
-{
-    int noofminers = 3;
-    puzzle();
-    fstream fileptr;
-    fstream filereward;
-    string filename = "miner";
-    string temp = to_string(miner);
-    string temp1 = ".txt";
-    filename += temp + temp1;
-    if (var == noofminers-1)
+    jar* pill(long& noofjars)
     {
-        fileptr.open(filename, ios::app);
-        fileptr << obj << endl;
-        fileptr.close();
-        filereward.open("rewards.txt", ios::app);
-        filereward << miner << endl;
-        filereward.close();
-    }
-    miner = -1;
-    answer = -1;
-    var = 0;
-}
-void takeinput(json &obj)
-{
-    fstream fileptr;int count=0;
-    fileptr.open("input.json", ios::in);
-    string time;
-    if (fileptr.is_open())
-    {
-        while (!fileptr.eof())
+        noofjars = misc.randoms(100000, 150000);
+        long nonce = misc.randoms(0, noofjars - 1);
+        jar* jars = new jar[noofjars];
+        for (long i = 0; i < noofjars;i++)
         {
-            time = NowToString();
-            fileptr >> obj;
-            obj["Time"] = time;
-            datapool(obj);
-            if (fileptr.peek()==EOF)
+            if (i == nonce)
             {
-                break;
+                jars[i].input(150000, 9);
+            }
+            else
+            {
+                jars[i].input(150000, 10);
             }
         }
+        return jars;
     }
-    fileptr.close();
+    void verify(jar* obj)
+    {
+        if (obj[answer - 1].pillsweight == 9)
+        {
+            var++;
+        }
+    }
+    void puzzlesolver(int noofminers)
+    {
+        long noofjars = 0;
+        jar* obj = pill(noofjars);
+        int x = misc.randoms(1, 4);
+        thread* temp = new thread[noofminers];
+        for (int i = 0;i < noofminers;i++)
+        {
+            temp[i] = std::thread(&puzzle::p1, this,obj, noofjars, i + 1);
+        }
+        for (int i = 0;i < noofminers; i++)
+        {
+            temp[i].join();
+        }
+    }
+}puzz;
+class mining
+{
+public:
+    friend class puzzle;
+    friend class miscellaneous;
+    int noofminers;
+    mining(int x)
+    {
+        noofminers = x;
+    }
+    void takeinput(json& obj)
+    {
+        fstream fileptr;int count = 0;
+        fileptr.open("input.json", ios::in);
+        string time;
+        if (fileptr.is_open())
+        {
+            while (!fileptr.eof())
+            {
+                time = misc.NowToString();
+                fileptr >> obj;
+                obj["Time"] = time;
+                datapool(obj);
+                if (fileptr.peek() == EOF)
+                {
+                    break;
+                }
+            }
+        }
+        fileptr.close();
+    }
+    void datapool(json& obj)
+    {
+        puzz.puzzlesolver(noofminers);
+        fstream fileptr;
+        fstream filereward;
+        string filename = "miner";
+        string temp = to_string(puzz.miner);
+        string temp1 = ".txt";
+        filename += temp + temp1;
+        if (puzz.var == noofminers - 1)
+        {
+            fileptr.open(filename, ios::app);
+            fileptr << obj << endl;
+            fileptr.close();
+            filereward.open("rewards.txt", ios::in);
 
-}
+            if (filereward.peek()==EOF)
+            {
+                filereward.close();
+                filereward.open("rewards.txt", ios::app);
+                filereward  << puzz.miner;
+                filereward.close();
+            }
+            else
+            {
+                filereward.close();
+                filereward.open("rewards.txt", ios::app);
+                filereward <<endl<< puzz.miner;
+                filereward.close();
+            }
+            
+        }
+        puzz.miner = -1;
+        puzz.answer = -1;
+        puzz.var = 0;
+    }
+    void getrewards(int x)
+    {
+        int temp, count = 0;
+        ifstream rewards("rewards.txt");
+        while (!rewards.eof())
+        {
+            rewards >> temp;
+            if (temp == x)
+            {
+                count++;
+            }
+        }
+        cout << "REWARDS OF MINER" << x << " : " << count;
+    }
+};
+
 int main()
 {
     json obj;
-    takeinput(obj);
+    mining m(3);
+   // m.takeinput(obj);
+    m.getrewards(1);
 }
